2780c053e5dd35859f97aac913a8b496bd598d25
Q Kim <yqkim91@gmail.com>
2020-06-01 03:33:29 -0400

    masonry layout initial iteration complete

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Inject } from '@angular/core';
 import { revealOnScrollAnimation, revealParallaxAnimation, slideUpFadeIn } from '../../shared/animations';
 import { DOCUMENT } from '@angular/common';
+import * as _ from "lodash";       //debugging
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,13 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./home.component.scss'],
   animations: [ revealOnScrollAnimation, revealParallaxAnimation, slideUpFadeIn ],
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit, AfterViewInit{
 
   private languages : Array<String>;
   private technologies : Array<String>;
   private databases : Array<String>;
   private tools : Array<String>;
+  private learning : Array<String>;
 
   public parallaxHeight: number;
   public visibleSections: Array<Object> = [];
@@ -22,6 +24,8 @@ export class HomeComponent implements OnInit{
   public showParallax : boolean;
 
   panelOpenState = false;
+
+  public allItems;
 
   constructor(@Inject(DOCUMENT) private document: Document) 
   { 
@@ -36,10 +40,12 @@ export class HomeComponent implements OnInit{
       this.parallaxHeight = document.documentElement.clientHeight - 56;
     }
 
-    this.languages = ["Java", "Spring", "C++", "Python"];
+    this.languages = ["Java", "Spring Boot", "C++"];
     this.technologies = ["HTML && SCSS", "Angular", "Node.js", "JavaScript && TypeScript"];
     this.databases=["MongoDB", "Logstash", "Filebeat"];
     this.tools=["Jenkins", "[Hashicorp] Consul, Nomad, Vault", "Elastic, Logstash, Kibana (ELK)"]
+
+    this.learning = ["Python", "ML"] ; 
 
     this.visibleSections.push({ "id": 0, "show": false });
     this.visibleSections.push({ "id": 1, "show": false });
@@ -92,27 +98,19 @@ export class HomeComponent implements OnInit{
 
     this.projectTiles = [
       {
+        "index":0,
         "title":"Personal Website V2",
         "description":"2nd iteration of my personal website built using MEAN (Angular 8)",
         "bulletPoints":[
-          "Followed Material Design UX guidelines",
-          "..."
+          "Material Design UX guidelines",
+          "Angular"
         ],
         "technologies":["Mongo, Express, Angular, Node.js"]
       },
       {
-        "title": "Content Management System",
-        "description":"A CMS for my blog, currently in Progress",
-        "technologies":["Mongo", "Spring", "In Progress"]
-      },
-      {
-        "title": "Simple Comment System",
-        "description":"A simple comment system for my blog",
-        "technologies":["Spring", "Mongo", "In Progress"]
-      },
-      {
-        "title": "Machine Learning Client",
-        "description":"Re-implemented various ML classification functions for learning purposes.",
+        "index":3,
+        "title": "Simple Machine Learning Client",
+        "description":"Re-implemented various ML classification algorithms for learning purposes.",
         "bulletPoints": [
           "Naive Bayes Classifier",
           "Perceptron (least square loss) Classifier",
@@ -122,14 +120,51 @@ export class HomeComponent implements OnInit{
         "technologies":["Angular", "Python", "Spring"]
       },
       {
+        "index":1,
+        "title": "Content Management System",
+        "description":"A CMS for my blog, currently in Progress",
+        "technologies":["Mongo", "Spring", "In Progress"]
+      },
+      {
+        "index":2,
+        "title": "Comment System",
+        "description":"A simple comment system for my blog",
+        "technologies":["Spring", "Mongo", "In Progress"]
+      },
+      {
+        "index":4,
         "title": "Simple Chat",
         "description":"Implemented a simple chat app with a material design based UI",
         "technologies":["Angular, Node.js, SocketIO"]
+      },
+      {
+        "index":4,
+        "title": "Affordable Care Act XML Form Generator",
+        "description":"Parses user generated excel data  XML based excel data. This is required when submitting more than 50 forms at once.",
+        "bulletPoints": [
+          "Generates XML forms for 1094-B, 1095-B, 1094-C, and 1095-C",
+          "XML format mandatory when employer files 1095B/1095C for more than 50 employees",
+          "https://www.irs.gov/e-file-providers/affordable-care-act-information-returns-air"
+        ],
+        "technologies":["Spring", ""]
       }
     ]
-  
+
     //LazyLoad for parallax bg
     this.showParallax = false;
+
+  }
+
+  ngAfterViewInit() {
+    //Masonry onload code
+    this.resizeAllGridItems();
+    window.addEventListener("resize", this.resizeAllGridItems);
+  
+    this.allItems = document.getElementsByClassName("masonry");
+    for(let x=0;x< this.allItems.length;x++){
+      this.resizeInstance(this.allItems[x]);
+      // this.imagesLoaded( this.allItems[x], this.resizeInstance);
+    }
   }
 
   loadParallax(someEvent) {
@@ -140,4 +175,32 @@ export class HomeComponent implements OnInit{
     let index = someEvent["index"];
     this.visibleSections[index]["show"]=someEvent["state"];
   }
+
+  /*
+    Masonry Style Test code below
+  */
+ resizeGridItem(item){
+   console.log("resizeGridItem() called");
+    let grid = document.getElementsByClassName("masonry")[0];
+    let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    let rowSpan = Math.ceil((item.querySelector('.masonItemContent').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+
+    item.style.gridRowEnd = "span "+rowSpan;
+  }
+
+  resizeAllGridItems(){
+    console.log("resizeAllGridItems() called");
+    let allItems = document.getElementsByClassName("masonItem");
+    for(let x=0;x<allItems.length;x++){
+      this.resizeGridItem(allItems[x]);
+    }
+  }
+
+  resizeInstance(instance){
+    console.log("resizeInstance() called");
+    let item = instance.elements[0];
+    this.resizeGridItem(item);
+  }
+
 }
